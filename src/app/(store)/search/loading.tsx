3dea903145e129398f64901a3559108d8ca4aed8
AdlerCastro <adlercastro01@gmail.com>
2024-08-10 20:40:49 -0300

    :bug: fix: Corrigindo erro para componente client side

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,18 +1,15 @@
 'use client'
 
 import { Skeleton } from '@/components/Skeleton'
-import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
+import { CurrentSearch } from './current-search'
 
 export default function SearchLoadign() {
-  const searchParams = useSearchParams()
-
-  const query = searchParams.get('q')
-
   return (
     <div className="flex flex-col gap-4">
-      <p className="text-sm">
-        Resultados para: <span className="font-semibold">{query}</span>
-      </p>
+      <Suspense>
+        <CurrentSearch />
+      </Suspense>
 
       <div className="grid grid-cols-3 gap-6">
         <Skeleton className="h-[360px]" />
